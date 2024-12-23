acf4f7713fda25f944e42ecb51ac9cbd555639d9
Andrey0800770 <andrey0800770@gmail.com>
2024-12-23 14:23:28 -0300

    fix warning: label ‘err2’ defined but not used

--- drivers/usb/dwc3/core.c
+++ drivers/usb/dwc3/core.c
@@ -1379,30 +1379,6 @@ static int dwc3_probe(struct platform_device *pdev)
 
 err_core_init:
 	dwc3_core_exit_mode(dwc);
-err5:
-	dwc3_event_buffers_cleanup(dwc);
-
-	usb_phy_shutdown(dwc->usb2_phy);
-	usb_phy_shutdown(dwc->usb3_phy);
-	phy_exit(dwc->usb2_generic_phy);
-	phy_exit(dwc->usb3_generic_phy);
-
-	usb_phy_set_suspend(dwc->usb2_phy, 1);
-	usb_phy_set_suspend(dwc->usb3_phy, 1);
-	phy_power_off(dwc->usb2_generic_phy);
-	phy_power_off(dwc->usb3_generic_phy);
-
-	dwc3_ulpi_exit(dwc);
-
-err4:
-	dwc3_free_scratch_buffers(dwc);
-
-err3:
-	dwc3_free_event_buffers(dwc);
-	dwc3_ulpi_exit(dwc);
-
-err2:
-	pm_runtime_allow(&pdev->dev);
 
 err1:
 	destroy_workqueue(dwc->dwc_wq);
